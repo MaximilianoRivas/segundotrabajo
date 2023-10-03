e8e40276f3b5972045dcae7941c8405277de28c1
Maximiliano Rivas <131488495+MaximilianoRivas@users.noreply.github.com>
2023-10-03 17:58:06 -0300

    Update Home.js
    arregle

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -40,37 +40,6 @@ export default function Home() {
 
 
 
-        /*
-        <header className='conteiner-header'>
-        <div className="conteiner">
-            <h1 className='titulo'>Proyect Rick & Morty</h1>
-            <h2 className='titulo'>Helcome to Rick & Morty Proyect!</h2>
-            <p className='parrafo'>This proyect was made for practising React and to made a functional website</p>
-
-           <p className='parrafo2'> In this website you can know information of the characters of this animated series.
-            <br/> Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion</p>
-            <h3 className='go'>Lets go!</h3>
-            <div className='hol'>
-            <nav className='row navba align-items-center'>
-
-            <ul className='nav nav-pills '>
-
-            <li className='nav-item'>
-
-            <Link to="/characters" className='botones'>Charactes</Link>
-            
-            </li>
-            <li className='nav-item'>
-            
-            <Link to="/contact" className='botones1'>Contact</Link>       
-            
-            
-            </li>
-            </ul>
-            </nav>
-            </div>      
-         </div>
-         </header>*/
-         
+       
     )
 }
